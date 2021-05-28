7dd84802399705e0be0b3cb2d25a6b30e5a35c98
megalom <megalom@list.ru>
2021-05-28 13:54:45 +0500

    update application

--- calculator/src/main/java/ru/megalomaniac/tasks/calculator/Application.java
+++ calculator/src/main/java/ru/megalomaniac/tasks/calculator/Application.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите пожалуйста операцию:");
         String input = scanner.nextLine();
         System.out.println("Результат вычисления: "+Calculator.calculateFromString(input));
+        scanner.close();
 
     }
 }
